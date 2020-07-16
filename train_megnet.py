c175baa7c2633b32d84d8534e0a298751959f94b
Alex Moriarty <amoriarty14@gmail.com>
2020-07-16 17:17:59 +0100

    Added MLflow tracking

--- train_megnet.py
+++ train_megnet.py
@@ -1,4 +1,6 @@
 """Train model to determine SSEs using MEGNet."""
+import mlflow
+import mlflow.tensorflow
 import numpy as np
 import pandas as pd
 import pyarrow.feather as feather
@@ -8,6 +10,9 @@ from megnet.models import MEGNetModel
 from sklearn.model_selection import train_test_split
 
 from .config import *
+
+mlflow.set_tracking_uri("databricks")
+mlflow.set_experiment("/Users/awm1017@ic.ac.uk/MEGNet SSE Model")
 
 nfeat_bond = 10
 r_cutoff = 5
@@ -32,19 +37,21 @@ train_structs, test_structs, train_sses, test_sses = train_test_split(
     structures, sse_tuples, random_state=2020
 )
 
-# Save the split we train with for use in the GP
-train_struc_strs = [struct.to("json") for struct in train_structs]
-test_struc_strs = [struct.to("json") for struct in test_structs]
+# * Save the split we train with for use in the GP
+# train_struc_strs = [struct.to("json") for struct in train_structs]
+# test_struc_strs = [struct.to("json") for struct in test_structs]
 
-train_df = data.loc[data["structure"].isin(train_struc_strs)]
-test_df = data.loc[data["structure"].isin(test_struc_strs)]
+# train_df = data.loc[data["structure"].isin(train_struc_strs)]
+# test_df = data.loc[data["structure"].isin(test_struc_strs)]
 
-feather.write_feather(train_df, DB_DIR / "train_df.fthr")
-feather.write_feather(test_df, DB_DIR / "test_df.fthr")
+# feather.write_feather(train_df, DB_DIR / "train_df.fthr")
+# feather.write_feather(test_df, DB_DIR / "test_df.fthr")
 
-# Model training
+# * Model training
 # Here, `structures` is a list of pymatgen Structure objects.
 # `targets` is a corresponding list of properties.
-# model.train(train_structs, train_sses, test_structs, test_sses, epochs=100)
+with mlflow.start_run(run_name="laptop_test"):
+    mlflow.tensorflow.autolog(every_n_iter=5)
+    model.train(train_structs, train_sses, test_structs, test_sses, epochs=100)
 
 # model.save_model("megnet_model_v1")
