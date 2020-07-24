5ac099eb4ebe54a03a0323663b4609ad7a2e5e6b
Alex Moriarty <amoriarty14@gmail.com>
2020-07-24 21:54:57 +0100

    Added layer scaling unit tests

---
+++ tests/test_datalib/test_preprocessing/test_layer_scaler.py
@@ -0,0 +1,33 @@
+"""Test layer scaling functionality."""
+import numpy as np
+import pandas as pd
+import pytest
+
+import sse_gnn.datalib.preprocessing as preproc
+
+eye3 = np.eye(3)
+max_elem_tests = [
+    # We expect a single array input to
+    # lead to return of input
+    ([eye3], eye3),
+    # Check for two arrays, three arrays, ... and five arrays
+    *[([eye3 * (j + 1) for j in range(i)], eye3 * i) for i in range(2, 6)],
+    # And now with some mixup between the different elements
+    ([eye3, 1 - eye3], np.ones((3, 3))),
+]
+
+
+@pytest.mark.parametrize("test_inp,expected", max_elem_tests)
+def test_get_max(test_inp, expected):
+    """Test `get_max_elements`."""
+    assert np.array_equal(preproc.get_max_elements(test_inp), expected)
+
+
+@pytest.fixture
+def trained_layer_scaler(mocker):
+    """Get a `LayerScaler` with a mock extractor."""
+    le = mocker.Mock()  # Layer extractor mock
+    # TODO: This function is WIP
+    le.get_layer_output.return_value = None
+    mocker.patch("sse_gnn.datalib.preprocessing.LayerExtractor")
+    model = mocker.Mock()
