7b99246e627543e59d81ac6ca6cc13e9e1ae9a71
Alex Moriarty <amoriarty14@gmail.com>
2020-08-04 10:46:09 +0100

    Added environment variables

--- examples/config.py
+++ examples/config.py
@@ -19,3 +19,12 @@ MP_API_VAR = "MPI_KEY"
 
 # Get the Materials Project API
 MP_API_KEY = os.environ.get(MP_API_VAR)
+
+N_EPOCHS = os.environ["N_EPOCHS"]  # Number of training epochs
+PATIENCE = os.environ["PATIENCE"]  # Patience
+PREV_MODEL = os.environ.get("PREV_MODEL")  # Location of the previous model
+NEW_MODEL = os.environ["NEW_MODEL"]  # Where to save the new model
+CKPT_PATH = os.environ.get("CKPT_PATH")  # Checkpoint path
+n_inducing = os.environ.get("NUM_INDUCING")  # How many inducing points for the VGP
+NUM_INDUCING = int(n_inducing) if n_inducing is not None else None
+RUN_NAME = os.environ.get("RUN_NAME")
