a26b663fa31fdbcabd576a2e9d712f9aa2e40102
Alex Moriarty <amoriarty14@gmail.com>
2020-08-06 12:13:34 +0100

    [WIP] MEGNet data preprocessing example

--- examples/meg_data_preproc.py
+++ examples/meg_data_preproc.py
@@ -1,19 +1,39 @@
 """Convert MEGNet pre-trained model data to layer outputs for a given model."""
 from megnet.utils.models import load_model
 from pyarrow import feather
+from sklearn.model_selection import train_test_split
 
-from sse_gnn.utils import serialize_array, deserialize_array
+from sse_gnn.datalib.preprocessing import LayerScaler, convert_graph_df
+from sse_gnn.utils import serialize_array
 
 from .config import DB_DIR
-from sse_gnn.datalib.preprocessing import LayerScaler
+
+
+TRAIN_OUT = DB_DIR / "train_megnet_dat.fthr"
+TEST_OUT = DB_DIR / "test_megnet_dat.fthr"
+
+
+def split_data(data):
+    """Split data into training and validation sets."""
+    return train_test_split(data, random_state=2020)
+
 
 model = load_model("Bandgap_MP_2018")
 
 data = feather.read_feather(DB_DIR / "parsed_megnet_dat.fthr")
 
-# Deserialize arrays
-for col in array_cols:
-    data[col] = data[col].apply(deserialize_array)
+train, test = split_data(data)
 
-scaler = LayerScaler(model, training_df=data,)
+scaler = LayerScaler(model, training_df=train, use_structs=False)
 
+with open(DB_DIR / "github_data_sf", "wb") as f:
+    scaler.sf.tofile(f)
+
+train = scaler.training_data
+test["layer_out"] = scaler.graphs_to_input(convert_graph_df(test))
+
+train["layer_out"] = train["layer_out"].apply(serialize_array)
+test["layer_out"] = test["layer_out"].apply(serialize_array)
+
+feather.write_feather(train, TRAIN_OUT)
+feather.write_feather(test, TEST_OUT)
