8cdeea5b492f86c076897c15331799d846a49ecf
Alex Moriarty <amoriarty14@gmail.com>
2020-08-19 17:27:41 +0100

    [WIP] Training routines

--- sse_gnn/base.py
+++ sse_gnn/base.py
@@ -1,13 +1,14 @@
 """Wrappers for main functionality of model training and saving."""
 from pathlib import Path
-from typing import Literal, List, Optional, Union
+from typing import Literal, List, Optional, Tuple, Union
 
 import numpy as np
 import pymatgen
+import tensorflow as tf
 from megnet.models import MEGNetModel
 
 from .datalib.preprocessing import LayerScaler
-from .gp.gp_trainer import GPTrainer
+from .gp.gp_trainer import GPTrainer, convert_index_points
 from .gp.vgp_trainer import SingleLayerVGP
 from .utilities.serialization import deserialize_array, serialize_array
 
@@ -24,6 +25,9 @@ class MEGNetProbModel:
         val_targets: The validation targets.
         ntarget: The number of target variables.
             This can only be greater than one if `gp_type` is 'VGP'.
+        layer_index: The index of the layer to extract outputs from
+            within :attr:`meg_model`. Defaults to the concatenation
+            layer.
         **kwargs: Keyword arguments to pass to :class:`MEGNetModel`.
 
     """
@@ -36,6 +40,8 @@ class MEGNetProbModel:
         val_structs: List[pymatgen.Structure],
         val_targets: List[np.ndarray],
         ntarget: int = 1,
+        layer_index: int = -4,
+        num_inducing_points: Optional[int] = None,
         **kwargs,
     ):
         """Initialize `MEGNetModel` and type of GP to use."""
@@ -45,6 +51,15 @@ class MEGNetProbModel:
             raise NotImplementedError(
                 f"Can only have `ntarget > 1` when `gp_type` is 'VGP' (got {ntarget=})"
             )
+        if gp_type == "GP" and num_inducing_points is not None:
+            raise ValueError(
+                "`num_inducing_points` can only be set when `gp_type` is `VGP`"
+            )
+        if gp_type == "VGP" and num_inducing_points is None:
+            raise ValueError(
+                "`num_inducing_points` must be supplied for `gp_type=VGP`, "
+                f"got {num_inducing_points=}"
+            )
 
         self.gp_type = gp_type
         self.meg_model = MEGNetModel(ntarget=ntarget, **kwargs)
@@ -52,6 +67,11 @@ class MEGNetProbModel:
         self.train_targets = train_targets
         self.val_structs = val_structs
         self.val_targets = val_targets
+        self.ntarget = ntarget
+        self.sf: Optional[np.ndarray] = None
+        self.layer_index = layer_index
+        self.gp: Optional[Union[GPTrainer, SingleLayerVGP]] = None
+        self.num_inducing_points = num_inducing_points
 
     def train_meg_model(
         self,
@@ -81,26 +101,99 @@ class MEGNetProbModel:
         if save_dir:
             self.meg_model.save_model(str(save_dir))
 
-    def get_index_points(self):
-        """Determine and preprocess index points for GP training."""
-        pass
+    def _update_sf(self):
+        """Update the saved scaling factor.
 
-    def train_uq(self, *args, **kwargs):
+        This must be called to update :attr:`sf` whenever the MEGNetModel
+        is updated (i.e. trained).
+
+        """
+        ls = LayerScaler.from_train_data(
+            self.meg_model, self.train_structs, layer_index=self.layer_index
+        )
+        self.sf = ls.sf
+
+    def get_index_points(
+        self, structures: List[pymatgen.Structure]
+    ) -> List[np.ndarray]:
+        """Determine and preprocess index points for GP training.
+
+        Args:
+            structures: A list of structrues to convert to inputs.
+
+        Returns:
+            index_points: The feature arrays of the structures.
+
+        """
+        ls = LayerScaler(self.meg_model, self.sf, self.layer_index)
+        return ls.structures_to_input(structures)
+
+    def train_uq(self, epochs: int = 500, **kwargs):
         """Train the uncertainty quantifier.
 
         Extracts chosen layer outputs from :attr:`meg_model`,
         scale them and train the appropriate GP (from :attr:`gp_type`).
 
         """
-        pass
+        training_idxs = np.stack(self.get_index_points(self.train_structs))
+        val_idxs = np.stack(self.get_index_points(self.val_structs))
 
-    def _train_gp(self, *args, **kwargs):
+        if self.gp_type == "GP":
+            training_idxs = convert_index_points(training_idxs)
+            val_idxs = convert_index_points(val_idxs)
+            self.gp = self._train_gp(training_idxs, val_idxs, epochs)
+        else:
+            training_idxs = tf.constant(training_idxs, dtype=tf.float64)
+            val_idxs = tf.constant(val_idxs, dtype=tf.float64)
+            self.gp = self._train_vgp(training_idxs, val_idxs, epochs)
+
+    def _train_gp(
+        self,
+        train_idxs: List[np.ndarray],
+        val_idxs: List[np.ndarray],
+        epochs: int,
+        **kwargs,
+    ):
         """Train a GP on preprocessed layer outputs from a model."""
-        pass
+        if self.gp_type != "GP":
+            raise ValueError("Can only train GP for `gp_type='GP'`")
 
-    def _train_vgp(self, *args, **kwargs):
+        gp_trainer = GPTrainer(train_idxs, self.train_targets)
+        metrics = list(gp_trainer.train_model(val_idxs, self.val_targets, epochs))
+        return gp_trainer
+
+    def _train_vgp(
+        self,
+        train_idxs: List[np.ndarray],
+        val_idxs: List[np.ndarray],
+        epochs: int,
+        **kwargs,
+    ):
         """Train a VGP on preprocessed layer outputs from a model."""
-        pass
+        if self.gp_type != "VGP":
+            raise ValueError("Can only train VGP for `gp_type='VGP'`")
+        if self.num_inducing_points is None:
+            # This should already have been handled in __init__, but just in case
+            raise ValueError("Cannot train VGP without `num_inducing_points`")
+
+        vgp = SingleLayerVGP(train_idxs, self.num_inducing_points, self.ntarget)
+        vgp.train_model(self.train_targets, (val_idxs, self.val_targets), epochs)
+        return vgp
+
+    def predict_structure(
+        self, struct: pymatgen.Structure
+    ) -> Tuple[np.ndarray, np.ndarray]:
+        """Predict target value and an uncertainty for a given structure.
+
+        Args:
+            struct: The structure to make predictions on.
+
+        Returns:
+            predicted_target: The predicted target value(s).
+            uncertainty: The uncertainty in the predicted value(s).
+
+        """
+        raise NotImplementedError()
 
     def save(self, fname: Union[Path, str]):
         """Save the full-stack model."""
