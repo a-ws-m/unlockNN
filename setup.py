fff759872de7894f7f75e8446cb4f765ec757165
a-ws-m <amoriarty14@gmail.com>
2020-09-16 13:36:12 +0100

    Change name to lowercase

--- setup.py
+++ setup.py
@@ -1,4 +1,32 @@
-"""Setup module discovery."""
-from setuptools import setup, find_packages
+"""Setup script for package."""
+import pathlib
+from setuptools import setup
 
-setup(name="sse_gnn", packages=find_packages())
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="unlockGNN",
+    version="1.0.0",
+    description="A Python package for interpreting and extracting uncertainties in graph neural network models of chemical systems based upon Gaussian processes.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/a-ws-m/unlockGNN",
+    author="Alexander Moriarty",
+    author_email="amoriarty14@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=["unlockgnn"],
+    include_package_data=True,
+    install_requires=[
+        # TODO: Add dependencies
+    ],
+)
