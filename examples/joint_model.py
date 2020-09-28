6ff73d93fd15593ced6087f6aa97d5ab652d482d
a-ws-m <amoriarty14@gmail.com>
2020-09-28 16:35:45 +0100

    Formatting + comments

--- examples/joint_model.py
+++ examples/joint_model.py
@@ -7,22 +7,18 @@ from megnet.data.crystal import CrystalGraph
 from sklearn.model_selection import train_test_split
 from unlockgnn import MEGNetProbModel
 
-# from unlockgnn.datalib.metrics import MetricAnalyser
-
-
 SAVE_DIR = Path.home() / "matbench_perovskites"
 DATASET = "matbench_perovskites"
 TARGET_VAR = "e_form"
 
 print(get_all_dataset_info(DATASET))
-
 data = load_dataset(DATASET)
-
 train_df, test_df = train_test_split(data, random_state=2020)
 
 print(f"Train DF:\n{train_df.describe()}")
 print(f"Test DF:\n{test_df.describe()}")
 
+# MEGNet parameters for crystal-to-graph conversion
 nfeat_bond = 100
 r_cutoff = 5
 gaussian_centers = np.linspace(0, r_cutoff + 1, nfeat_bond)
@@ -35,6 +31,7 @@ meg_args = {
     "metrics": ["mae"],
 }
 
+# Model construction
 prob_model = MEGNetProbModel(
     train_df["structure"],
     train_df[TARGET_VAR],
