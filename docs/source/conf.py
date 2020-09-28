0c3fdaa1c1bd08fe8f3854d2b48974c2ec6da3d3
a-ws-m <amoriarty14@gmail.com>
2020-09-28 16:43:48 +0100

    Added autosectionlabel

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,11 @@ author = "Alexander Moriarty"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.napoleon", "sphinx.ext.doctest"]
+extensions = [
+    "sphinx.ext.napoleon",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autosectionlabel",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
