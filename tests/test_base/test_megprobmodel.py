91e41e482ca8dbbcee099f2a158be29192598197
Alex Moriarty <amoriarty14@gmail.com>
2021-02-17 16:25:50 +0000

    [FIX] New GP kernel specification

--- tests/test_base/test_megprobmodel.py
+++ tests/test_base/test_megprobmodel.py
@@ -95,8 +95,8 @@ def test_train(tmp_path, mocker, structure_database, gp_type, n_inducing):
     NEW_GP_SAVE_DIR = SAVE_DIR / "new_gp"
 
     new_kernel_layer = unlockgnn.gp.kernel_layers.MaternOneHalfFn()
-    # Need to extract the kernel property if we're using a GP
-    new_kernel = new_kernel_layer if gp_type == "VGP" else new_kernel_layer.kernel
+    # Need to extract the kernel property if our new model is a GP
+    new_kernel = new_kernel_layer if gp_type == "GP" else new_kernel_layer.kernel
 
     new_gp_model = prob_model.change_gp_type(new_kernel, NEW_GP_SAVE_DIR)
 
