bb94ca6ad992f4ea4634653eb087e986adb0f815
a-ws-m <amoriarty14@gmail.com>
2021-09-02 17:38:18 +0100

    Update patch version to reflect name change

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="unlockNN",
-    version="2.0.0",
+    version="2.0.1",
     description="Uncertainty quantification for neural network models of chemical systems.",
     long_description=README,
     long_description_content_type="text/markdown",
